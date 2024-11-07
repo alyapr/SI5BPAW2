bd097258cb7b01f6550094d4393b845d43464fe9
alyapr <putririzaldyalya@gmail.com>
2024-11-07 13:05:25 +0700

    post create

---
+++ prjAngular1/src/app/post-create/post-create.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-post-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-create.component.html',
+  styleUrl: './post-create.component.css'
+})
+export class PostCreateComponent {
+  inputPost="";
+  newPost="Inisialisasi Variabel";
+  //String interpolation {{}}
+
+  onSavePost(){
+    // alert("Hello Dunia")
+    this.newPost=this.inputPost
+  }
+}
