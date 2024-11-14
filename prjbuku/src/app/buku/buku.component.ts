64a5f8aa6b3243728847408c95789e9eafb95dc0
alyapr <putririzaldyalya@gmail.com>
2024-11-14 19:56:13 +0700

    perbaikan buku component html

--- prjbuku/src/app/buku/buku.component.ts
+++ prjbuku/src/app/buku/buku.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+
 @Component({
   selector: 'app-buku',
   templateUrl: './buku.component.html',
   styleUrl: './buku.component.css'
 })
 export class BukuComponent {
+
   simpanBuku(form : NgForm){
     console.log("Pengujian Klik")
     console.log(form.value.judul);
