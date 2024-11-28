3f379b5749c629398613ad199151bc27842558d3
alyapr <putririzaldyalya@gmail.com>
2024-11-28 13:06:13 +0700

    update buku component ts

--- prjbuku/src/app/services/buku.service.ts
+++ prjbuku/src/app/services/buku.service.ts
@@ -1,29 +1,69 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Buku } from '../models/buku.model';
-import { response } from 'express';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BukuService {
-  private url : string="https://localhost:3000/buku"
+  private url : string= "https://apisi51.vercel.app/buku/";
+  // private url : string = "https://localhost:3000"
+
+  private subjectBuku = new Subject<Buku[]>();
+  private subjectExecute = new Subject<string>;
 
   constructor(private http : HttpClient) { }
 
+  executeBukuListener(){
+    return this.subjectExecute.asObservable();
+  }
+
+  getBukuListener(){
+    return this.subjectBuku.asObservable();
+  }
+
+  getBuku(){
+    this.http.get<{message : string, bukus : Buku[]}>(this.url)
+    .subscribe((value)=>{
+      this.subjectBuku.next(value.bukus);
+    });
+  }
+
   addBuku(judul : string, penulis : string, genres : string[]){
-    const buku :  Buku={
+    const buku : Buku = {
       _id : null,
       judul : judul,
       penulis : penulis,
-      genre : genres 
+      genre : genres
+    }
+    
+    //console.log(buku);
+
+    this.http.post<{message : string}>(this.url,buku)
+    .subscribe((response)=>{
+      this.getBuku();
+      this.subjectExecute.next(response.message);
+      //console.log(response.message)
+    });
+  }
+
+  deletebuku(buku : Buku){
+    this.http.delete<{message : string}>(this.url + buku._id)
+    .subscribe((response)=>{
+      //console.log(response.message);
+      this.getBuku();
+      this.subjectExecute.next(response.message);
+    });
+  }
+
+  updateBuku(judul : string, penulis : string, genres : string[],id : string){
+    const buku : Buku = {
+      _id : id,
+      judul : judul,
+      penulis : penulis,
+      genre : genres
     }
 
-    this.http.post<{message : string}> (this.url, buku)
-    .subscribe((response)=>{
-      console.log(response.message)
-    });
-
   }
-  
 }
